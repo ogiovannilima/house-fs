58ad33c4d91926c30fc31ed43eca3020cd1ba21a
Giovanni Lima <giovannilimadev@gmail.com>
2024-11-07 16:01:54 -0300

    add a lot padding etst

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  padding: 5rem 1rem;
+  padding: 0 1rem 5rem 1rem;
 `;
 
 export const Content = styled.div`
