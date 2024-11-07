e338fa5615bff4e47a7afc13b171a77eca4ead2a
Giovanni Lima <giovannilimadev@gmail.com>
2024-11-07 15:46:42 -0300

    test remove height

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,7 +7,6 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  height: 100%;
   width: 100%;
   display: flex;
   align-items: center;
@@ -15,13 +14,11 @@ export const Content = styled.div`
 `;
 export const PageOne = styled.section`
   width: 100%;
-  height: 100%;
   display: flex;
   align-items: center;
 `;
 
 export const NavBar = styled.div`
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
