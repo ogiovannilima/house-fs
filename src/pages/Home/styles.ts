0d3db1b76d01de8449f4ba3608c1f08a09f25e1d
Giovanni Lima <giovannilimadev@gmail.com>
2024-11-07 15:41:37 -0300

    teste remove padding 1541

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-  padding: 3rem 1rem;
+  padding: 1rem;
 `;
 
 export const Content = styled.div`
