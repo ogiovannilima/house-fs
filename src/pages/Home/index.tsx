4061ca331867322b46c37597dfe6794855ce0892
Giovanni Lima <giovannilimadev@gmail.com>
2024-11-07 18:10:35 -0300

    add page two

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ import {
   BackgroundImage,
   Container,
   Content,
+  DescriptionPage,
   ImageWrapper,
   LanguageMenu,
   LanguageMenuItem,
@@ -19,8 +20,16 @@ import {
   MenuItem,
   NavBar,
   PageOne,
+  PageTwo,
+  QuoteBar,
+  QuoteTitle,
   SocialBar,
   SocialBarItem,
+  SubTitlePage,
+  TextContainer,
+  TextQuoteBox,
+  TextQuoteItem,
+  TitlePage,
 } from "./styles";
 
 import { LogoImg, WomankidImg } from "../../assets";
@@ -111,6 +120,64 @@ export function Home() {
             </SocialBar>
           </NavBar>
         </PageOne>
+        <PageTwo>
+          <TitlePage>Nuestro Compromiso</TitlePage>
+          <TextContainer>
+            <SubTitlePage>
+              Si combinamos la creatividad humana, la pasión y la experiencia
+              con lo mejor de la tecnología, podemos ir más allá. Algo que
+              impacte la vida de las personas y se sienta en el mundo a su
+              alrededor.
+            </SubTitlePage>
+            <SubTitlePage>Construyendo confianza, hoy y mañana.</SubTitlePage>
+
+            <DescriptionPage>
+              En House Facility, entendemos la importancia de brindar un
+              servicio excepcional tanto a las aseguradoras como a sus
+              asegurados. Nuestra filosofía se basa en un servicio
+              personalizado, enfocado en la resolución eficiente y eficaz de
+              siniestros, garantizando así el más alto nivel de bienestar y
+              satisfacción para el asegurado. Este enfoque no solo mejora la
+              experiencia del cliente final, sino que también refuerza la
+              confianza y la lealtad hacia su aseguradora.
+            </DescriptionPage>
+
+            <DescriptionPage>
+              Somos una empresa dedicada a la gestión de Facility Services, con
+              más de 15 años de experiencia y presencia consolidada en América
+              Latina y Asia. Nuestra misión es ofrecer soluciones integrales
+              para la gestión de siniestros, así como servicios de
+              mantenimiento, limpieza, seguridad, eficiencia energética, gestión
+              de residuos y otros servicios esenciales para el buen
+              funcionamiento de los espacios. Nos enfocamos en la
+              sostenibilidad, alineándonos con los principios de ESG (Ambiental,
+              Social y Gobernanza), y garantizamos entornos que promuevan el
+              bienestar y la seguridad de las personas.
+            </DescriptionPage>
+          </TextContainer>
+
+          <TextQuoteBox>
+            <TextQuoteItem>
+              <QuoteBar />
+              <span>
+                <QuoteTitle>Misión</QuoteTitle>
+                Ofrecer soluciones completas e innovadoras en gestión de
+                facilidades, promoviendo entornos saludables y sostenibles para
+                personas y empresas, gobierno, espacios corporativos y
+                comerciales, aeropuertos, industriales y residenciales.
+              </span>
+            </TextQuoteItem>
+            <TextQuoteItem>
+              <QuoteBar />
+              <span>
+                <QuoteTitle>Visión</QuoteTitle>
+                Ser reconocidos como una empresa líder en Facility services,
+                comprometida con la excelencia, la innovación y el impacto
+                positivo en el medioambiente y la sociedad.
+              </span>
+            </TextQuoteItem>
+          </TextQuoteBox>
+        </PageTwo>
       </Content>
     </Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -144,3 +144,96 @@ export const BackgroundImage = styled.div`
     }
   }
 `;
+
+export const PageTwo = styled.section`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-top: 2rem;
+
+  @media (min-width: 1025px) {
+    // Para desktop
+    padding-top: 10rem;
+  }
+`;
+
+export const TextContainer = styled.div`
+  margin: 0 auto;
+  max-width: 80vw;
+
+  @media (min-width: 481px) {
+    // Para tablet
+    max-width: 70vw;
+  }
+
+  @media (min-width: 1025px) {
+    // Para desktop
+    max-width: 60vw;
+  }
+`;
+
+export const TitlePage = styled.div`
+  margin: 0 auto;
+  font-size: 1rem;
+  font-weight: 700;
+  color: #009a57;
+  text-transform: uppercase;
+`;
+
+export const SubTitlePage = styled.div`
+  text-align: center;
+  padding: 1rem 0;
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+export const DescriptionPage = styled.div`
+  text-align: justify;
+  padding: 1rem 0;
+  font-size: 1rem;
+`;
+
+export const TextQuoteBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 2rem;
+
+  @media (min-width: 481px) {
+    // Para tablet
+    flex-direction: row;
+    max-width: 90vw;
+  }
+
+  @media (min-width: 1025px) {
+    // Para desktop
+  }
+`;
+
+export const TextQuoteItem = styled.div`
+  display: flex;
+  margin: 0 0 2rem 0;
+  font-size: 1rem;
+  font-style: italic;
+
+  span {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const QuoteBar = styled.div`
+  background-color: #009a57;
+  width: 20px;
+  height: 100%;
+  margin-right: 0.5rem;
+`;
+
+export const QuoteTitle = styled.div`
+  padding: 0 0 0.5rem 0;
+  font-size: 1rem;
+  font-weight: 600;
+  font-style: normal;
+`;
